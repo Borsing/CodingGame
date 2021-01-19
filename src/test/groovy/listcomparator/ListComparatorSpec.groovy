679e0935d75543668cd0acf2c5ef35d91fa58910
benjamin-robert-cbp <benjamin.robert@cbp-group.com>
2021-01-19 14:55:51 +0100

    [ListComparator] Comment test cases.

--- src/test/groovy/listcomparator/ListComparatorSpec.groovy
+++ src/test/groovy/listcomparator/ListComparatorSpec.groovy
@@ -3,6 +3,9 @@ package listcomparator
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import java.util.stream.Collectors
+import java.util.stream.IntStream
+
 class ListComparatorSpec extends Specification {
 
     @Unroll
@@ -14,30 +17,30 @@ class ListComparatorSpec extends Specification {
         result == expectedResult
 
         where:
-        testCase          | listA                | listB                | expectedResult
-        "Null"            | null                 | null                 | false
-        "None"            | []                   | []                   | true
+        testCase          | listA                  | listB                  | expectedResult
+        "Null"            | null                   | null                   | false
+        "None"            | []                     | []                     | true
 
-        "Simple Case"     | ["A"]                | ["A"]                | true
-        "Simple Case 2"   | ["A"]                | ["B"]                | false
+        "Simple Case"     | ["A"]                  | ["A"]                  | true
+        "Simple Case 2"   | ["A"]                  | ["B"]                  | false
 
-        "Same Order"      | ["A", "B", "C"]      | ["A", "B", "C"]      | true
-        "Different Order" | ["A", "B", "C"]      | ["C", "B", "A"]      | true
+        "Same Order"      | ["A", "B", "C"]        | ["A", "B", "C"]        | true
+        "Different Order" | ["A", "B", "C"]        | ["C", "B", "A"]        | true
 
-        "Bad typing"      | ["A", "B", "C"]      | ["A", "D", "C"]      | false
-        "Intruder"        | ["A", "B", "C"]      | ["A", "B", "C", "D"] | false
-        "Intruder 2"      | ["A", "B", "C", "D"] | ["A", "B", "C"]      | false
+        "Bad typing"      | ["A", "B", "C"]        | ["A", "D", "C"]        | false
+        "Intruder"        | ["A", "B", "C"]        | ["A", "B", "C", "D"]   | false
+        "Intruder 2"      | ["A", "B", "C", "D"]   | ["A", "B", "C"]        | false
 
-        "Duplicated"      | ["A", "B", "C"]      | ["A", "A", "B", "C"] | true
-        "Duplicated 2"    | ["A", "A", "B", "C"] | ["C", "B", "A"]      | true
+        "Duplicated"      | ["A", "B", "C"]        | ["A", "A", "B", "C"]   | true
+        "Duplicated 2"    | ["A", "A", "B", "C"]   | ["C", "B", "A"]        | true
 
-//        "Other type"      | [1, 2, 3]               | [1, 2, 3]               | true
+        "Other type"      | [1, 2, 3]              | [1, 2, 3]              | true
 
 //        "Limits"          | listRange(0, 10000000) | listRange(1, 10000000) | false
 //        "Limits"          | listRange(0, 10000000) | listRange(0, 10000000) | true
     }
 
-//    private static List<Integer> listRange(int min, int max) {
-//        return IntStream.range(min, max).boxed().collect(Collectors.toList())
-//    }
+    private static List<Integer> listRange(int min, int max) {
+        return IntStream.range(min, max).boxed().collect(Collectors.toList())
+    }
 }
