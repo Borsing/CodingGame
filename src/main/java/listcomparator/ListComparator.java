0a6255765dbe33e33eab08aa60be74d0ab6fe801
benjamin-robert-cbp <benjamin.robert@cbp-group.com>
2021-01-19 15:47:08 +0100

    [ListComparator] Add javadoc.

--- src/main/java/listcomparator/ListComparator.java
+++ src/main/java/listcomparator/ListComparator.java
@@ -4,6 +4,20 @@ import java.util.List;
 
 public class ListComparator {
 
+	/**
+	 * the two lists have exactly the same elements of each other.
+	 *  - We don’t care about the order. 
+	 *  - We don’t care if a list has duplicate elements as long as the other list has at least one of these elements. 
+	 *
+	 * Example :
+	 * [“A” , “A”, “B”, “C” ] and [ “C”, “A”, “B”] have the same elements.
+	 * [“A” , “C” ] and [ “C”, “B”, “A”] doesn’t, because “B” isn’t present in the first list.
+	 *
+	 * You can help you with the Spec file ListComparatorSpec. 
+	 * The two lists can have 10 000 000 elements and the function must respond in less of 2 secs. 
+	 * Your function must be optimized.
+	 *
+	 */
 	public static <T> boolean haveSameElements(List<T> list1, List<T> list2) {
 		return false;
 	}
