4c3ce6045a83cb233eb85af6b0237a7b8d575890
benjamin-robert-cbp <benjamin.robert@cbp-group.com>
2021-01-19 14:40:58 +0100

    [ListComparator] Create test.

---
+++ src/main/java/listcomparator/ListComparator.java
@@ -0,0 +1,10 @@
+package listcomparator;
+
+import java.util.List;
+
+public class ListComparator {
+
+	public static boolean haveSameElements(List<String> list1, List<String> list2) {
+		return false;
+	}
+}
---
+++ src/test/groovy/listcomparator/ListComparatorSpec.groovy
@@ -0,0 +1,34 @@
+package listcomparator
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class ListComparatorSpec extends Specification {
+
+    @Unroll
+    void "Test ListComparator haveSameElements for the give #testCase"() {
+        when:
+        def result = ListComparator.haveSameElements(listA, listB)
+
+        then:
+        result == expectedResult
+
+        where:
+        testCase          | listA                | listB                | expectedResult
+        "Null"            | null                 | null                 | false
+        "None"            | []                   | []                   | true
+
+        "Simple Case"     | ["A"]                | ["A"]                | true
+        "Simple Case 2"   | ["A"]                | ["B"]                | false
+
+        "Same Order"      | ["A", "B", "C"]      | ["A", "B", "C"]      | true
+        "Different Order" | ["A", "B", "C"]      | ["C", "B", "A"]      | true
+
+        "Bad typing"      | ["A", "B", "C"]      | ["A", "D", "C"]      | false
+        "Intruder"        | ["A", "B", "C"]      | ["A", "B", "C", "D"] | false
+        "Intruder 2"      | ["A", "B", "C", "D"] | ["A", "B", "C"]      | false
+        
+        "Duplicated"      | ["A", "B", "C"]      | ["A", "A", "B", "C"] | true
+        "Duplicated 2"    | ["A", "A", "B", "C"] | ["C", "B", "A"]      | true
+    }
+}
